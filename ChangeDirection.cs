153b60113611275e8141cda4ad78ec631b40fc4e
dato818 <57722526+dato818@users.noreply.github.com>
2020-05-18 21:59:20 +0400

    Add files via upload

---
+++ ChangeDirection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDirection : MonoBehaviour
+{
+   public bool moveRight;
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveRight){
+           
+            rb.velocity = new Vector2 (playerSpeed, rb.velocity.y);
+        } else {
+           
+            rb.velocity = new Vector2 (-playerSpeed, rb.velocity.y);
+        }
+
+         }
+
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name == "Wall"){
+             moveRight = !moveRight;
+        }
+    }
+}
---
+++ PerfectJump.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerfectJump : MonoBehaviour
+{
+       public Vector2 jumpForce;
+    [SerializeField]
+    private float gravityScale = 1f;
+  [SerializeField]
+    public float jumpTimer = 0.5f;
+    public bool grounded;
+ public float jumpTime = 0f;
+    public float jumpDelay = 0.5f;
+    public float jumpDirection;
+private bool pressedJump = false;
+    private bool releasedJump = false;
+// private bool grounded;
+    
+    public LayerMask whatIsGround;
+
+
+     private bool startTimer = false;
+    private float timer;
+  
+          private Collider2D myCollider;
+
+
+   // public AudioSource jumpSound;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+         timer = jumpTimer;
+
+    rb = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+          if (Input.GetMouseButtonDown(0))
+        {
+            jumpTime = Time.time + jumpDelay;
+    }
+
+    if (Input.GetMouseButtonUp(0))
+        {
+            releasedJump = true;
+        }
+
+
+        if (startTimer) {
+            timer -= Time.deltaTime;
+            if (timer <= 0) {
+                releasedJump = true;
+            }
+        }
+    }
+
+
+
+     private void FixedUpdate() {
+        if(jumpTime > Time.time && grounded) {
+            pressedJump = true;
+           // jumpSound.Play();
+        }
+     
+        
+
+        if (pressedJump) {
+            StartJump();
+        }
+
+        if (releasedJump) {
+            StopJump();
+        }
+    }
+
+
+    private void StartJump() {
+        rb.gravityScale = 0;
+        GetComponent<Rigidbody2D>().AddForce(jumpForce, ForceMode2D.Impulse);
+        pressedJump = false;
+        startTimer = true;
+        jumpTime = 0;
+    }
+
+    private void StopJump() {
+        rb.gravityScale = gravityScale;
+        releasedJump = false;
+        timer = jumpTimer;
+        startTimer = false;
+    }
+
+
+}
